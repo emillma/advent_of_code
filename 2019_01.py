abdbb60e842a815534cf1e2e720e41c2390b7a8b
Emil Martens <emil.martens@gmail.com>
2020-12-01 12:31:11 +0100

    2019_1

---
+++ 2019_01.py
@@ -0,0 +1,26 @@
+# from utils import submit, get_data
+
+from utils import get_data, submit
+
+data = get_data(1, 2019)
+data = [int(i) for i in data.splitlines()]
+
+
+def foo(number):
+    return number//3 - 2
+
+
+svar_a = sum(foo(i) for i in data)
+
+
+def bar(number):
+    total = foo(number)
+    previous = foo(total)
+    while previous > 0:
+        total += previous
+        previous = foo(previous)
+    return total
+
+
+svar_b = sum(bar(i) for i in data)
+submit(svar_b, 1, 2019)
---
+++ utils.py
@@ -0,0 +1,22 @@
+import aocd
+import os
+from private import session
+import csv
+
+
+def get_data(day=None, year=None):
+    filename = os.path.join('data', f'{year}_{day}.txt')
+    if not os.path.isfile(filename):
+        data = aocd.get_data(session, day, year)
+        with open(filename, 'w') as file:
+            file.write(data)
+    else:
+        with open(filename, 'r') as file:
+            data = file.read()
+
+    return data
+
+
+def submit(answer, day=None, year=None, part=None):
+    aocd.submit(answer=answer, part=part, day=day, year=year, session=session,
+                reopen=True, quiet=False)
